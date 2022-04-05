786460d35c6ce23d857ada9d3ac11d5b08aae6ff
rDelgC <rdelgado.dev@gmail.com>
2022-04-05 16:58:35 -0500

    Updated

--- src/main/java/com/boot/bansis/transaction/BansisTransactionApplication.java
+++ src/main/java/com/boot/bansis/transaction/BansisTransactionApplication.java
@@ -2,10 +2,12 @@ package com.boot.bansis.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
 @EnableReactiveMongoAuditing
+@EnableEurekaClient
 public class BansisTransactionApplication {
 
 	public static void main(String[] args) {
